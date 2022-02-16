cddb11099fd18675838cc37958a27a71f51d5971
Aro Parada <haroldparada20@gmail.com>
2022-02-15 19:27:50 -0500

    added on click

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,24 @@
 import ReactDOM from 'react-dom';
+import { useState } from 'react';
+
 
 const App = () => {
-    return <h1>
-        hi
-    </h1>
-}
+    const [input, setInput] = useState('');
+    const [code, setCode] = useState('');
+
+    const onClick = () => {
+        console.log(input);
+    }
+
+
+    return <div>
+        <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
+        <div>
+            <button onClick={onClick}>submit</button>
+        </div>
+        <pre>{code}</pre>
+    </div>;
+};
 
 ReactDOM.render(
     <App />,
