4fa8bcb5413537937b8beb60436e7a7d8c09ff87
Aro Parada <haroldparada20@gmail.com>
2022-06-01 15:57:37 -0400

    added communication from the Cli to the API

--- packages/cli/dist/commands/serve.js
+++ packages/cli/dist/commands/serve.js
@@ -2,11 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serveCommand = void 0;
 const commander_1 = require("commander");
+const local_api_1 = require("local-api");
 exports.serveCommand = new commander_1.Command()
     //square brackets tell commander its a optional value & angle brackets are required
     .command('serve [filename]')
     .description('Open a file for editing')
     .option('-p, --port <number>', 'port to run server on', '4005')
     .action((filename = 'notebook.js', options) => {
-    console.log(filename, options);
+    (0, local_api_1.serve)(parseInt(options.port), filename, '/');
 });
