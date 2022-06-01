a8ab2da1a7c90953f340bdc79446e4f4a58484b3
Aro Parada <haroldparada20@gmail.com>
2022-06-01 17:18:17 -0400

    handled port errors

--- packages/cli/node_modules/local-api/dist/index.js
+++ packages/cli/node_modules/local-api/dist/index.js
@@ -4,11 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
-const express_1 = __importDefault(require("express"));
-const serve = (port, filename, dir) => {
-    const app = (0, express_1.default)();
-    app.listen(port, () => {
-        console.log('listening on port', port);
+var express_1 = __importDefault(require("express"));
+var serve = function (port, filename, dir) {
+    var app = express_1.default();
+    return new Promise(function (resolve, reject) {
+        app.listen(port, resolve).on('error', reject);
     });
 };
 exports.serve = serve;
--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,13 +1,24 @@
 import path from 'path';
 import { Command } from 'commander';
-import { serve } from 'local-api'
+import { serve } from 'local-api';
 
 export const serveCommand = new Command()
-//square brackets tell commander its a optional value & angle brackets are required
- .command('serve [filename]')
- .description('Open a file for editing')
- .option('-p, --port <number>', 'port to run server on', '4005' )
- .action((filename = 'notebook.js', options: { port: string }) => {
-    const dir = path.join(process.cwd(), path.dirname(filename)) 
-     serve(parseInt(options.port), path.basename(filename), dir);  
- });
+  .command('serve [filename]')
+  .description('Open a file for editing')
+  .option('-p, --port <number>', 'port to run server on', '4005')
+  .action(async (filename = 'notebook.js', options: { port: string }) => {
+    try {
+      const dir = path.join(process.cwd(), path.dirname(filename));
+      await serve(parseInt(options.port), path.basename(filename), dir);
+      console.log(
+        `Opened ${filename}. Navigate to http://localhost:${options.port} to edit the file.`
+      );
+    } catch (err) {
+      if (err.code === 'EADDRINUSE') {
+        console.error('Port is in use. Try running on a different port.');
+      } else {
+        console.log('Heres the problem', err.message);
+      }
+      process.exit(1);
+    }
+  });
